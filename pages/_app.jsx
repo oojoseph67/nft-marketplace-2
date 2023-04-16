11377ddeee15ccff70dbeb34433e475585e7bb45
Obinna Okolo <oojoseph67@gmail.com>
2023-04-16 23:55:50 +0100

    first commit

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,12 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react"
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }) {
   return (
